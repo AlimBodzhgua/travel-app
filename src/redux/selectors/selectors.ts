0a3ef1f53113a3e31f4840c81b9633117d64b6c0
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-07-30 18:05:37 +0300

    added user selector function

--- src/redux/selectors/selectors.ts
+++ src/redux/selectors/selectors.ts
@@ -1,5 +1,9 @@
 import {RootState} from 'redux/store';
 
+export const selectUser = (state: RootState) => {
+	return state.userReducer.user;
+}
+
 export const selectTravelById = (state: RootState, id: number) => {
 	return state.userReducer.user?.travels.find(travel => travel.id === Number(id))	
 }
