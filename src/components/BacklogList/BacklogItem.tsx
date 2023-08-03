6fcc12ec1fc3f07d81af375e1dfc8f2079e98965
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-03 18:09:41 +0300

    deleted useDrop fro backlogItem

--- src/components/BacklogList/BacklogItem.tsx
+++ src/components/BacklogList/BacklogItem.tsx
@@ -3,8 +3,7 @@ import {IBacklog} from 'types/types';
 import {useParams} from 'react-router-dom';
 import {useAppDispatch} from 'hooks/redux';
 import {userSlice} from 'redux/reducers/userSlice';
-import {useDrag, useDrop} from 'react-dnd';
-import { XYCoord } from 'react-dnd';
+import {useDrag} from 'react-dnd';
 import classes from './backlog.module.css';
 
 interface BacklogItemProps {
@@ -16,7 +15,6 @@ const BacklogItem: FC<BacklogItemProps> = ({backlog}) => {
 	const [editable, setEditable] = useState<boolean>(false);
 	const { id } = useParams<{id?: string}>();
 	const inputRef = useRef<HTMLInputElement>(null);
-	const itemRef = useRef<HTMLLIElement>(null);
 	const dispatch = useAppDispatch();
 
 	const [{isDragging}, drag] = useDrag(() => ({
@@ -27,46 +25,7 @@ const BacklogItem: FC<BacklogItemProps> = ({backlog}) => {
 		})
 	}))
 
-	const [{isOver}, drop] = useDrop<
-		IBacklog, 
-		void,
-		{isOver: boolean}
-	>(() => ({
-		accept: 'backlog',
-		collect: (monitor) => ({
-			isOver: monitor.isOver()
-		}),
-		hover: (item, monitor) => {
-			if (!itemRef.current) {
-				return;
-			}
-			const dragId = item.id;
-			const hoverId = backlog.id;
-
-			if (dragId === hoverId) {
-				return;
-			}
-
-			// Determine rectangle on screen
-			const hoverBoundingRect = itemRef.current?.getBoundingClientRect(); 
-			//Get vertical middle
-			const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
-			// Determine mouse position
-			const clientOffset = monitor.getClientOffset()
-
-			const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
-
-			if (dragId < hoverId && hoverClientY < hoverMiddleY) {
-				return;
-			}
-
-			// Dragging upwards
-			if (dragId > hoverId && hoverClientY > hoverMiddleY) {
-				return;
-			}
-		}
-	}))
-
+	
 	useEffect(() => {
 		if (editable === true) {
 			inputRef.current?.focus();
@@ -92,12 +51,11 @@ const BacklogItem: FC<BacklogItemProps> = ({backlog}) => {
 		setEditable(false);
 	}
 
-	drag(drop(itemRef))
 
 	return (
 		<li 
 			className={classes.item}
-			ref={itemRef}
+			ref={drag}
 			style={{
 				transform: isDragging ? 'scale(1.1)' : 'scale(1)',
 				opacity: isDragging ? '0' : '1',
