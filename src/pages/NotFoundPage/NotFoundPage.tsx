520afec7a8b909ff856044d88f218e6acc02d268
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-10 15:27:23 +0300

    added css and link to home page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,18 @@
 import {FC} from 'react';
+import {NavLink} from 'react-router-dom';
 import classes from './not-found.module.css'
 
 const NotFoundPage: FC = () => {
 	return (
-		<div>
-			<h1>NotFoundPage</h1>
+		<div className={classes.centered}>
+			<h1>
+				Page not found.<br/>
+				Such page does not exist or not available if you not logged in
+			</h1>
+			<NavLink 
+				to='/'
+				className={classes.link}
+			>Go to home page</NavLink>
 		</div>
 	)
 }
