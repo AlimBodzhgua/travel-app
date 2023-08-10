af4c23017f46802ba809039901b7453e8b1acaba
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-10 12:28:48 +0300

    added styles and links

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,41 @@
 import {FC} from 'react';
 import {useAppSelector} from 'hooks/redux';
-import {selectUser} from 'redux/selectors/selectors';
+import {NavLink} from 'react-router-dom';
+import NavBar from 'components/Navbar/NavBar';
+import classes from './home.module.css';
 
 const HomePage: FC = () => {
-	const user = useAppSelector(selectUser);
+	const {isAuth} = useAppSelector(state => state.userReducer);
 
 	return (
-		<div>
-			HomePage
+		<div className={classes.container}>
+			<NavBar />
+			<div className={classes.centered}>
+				{isAuth 
+					?
+						<>
+							<NavLink 
+								to='/profile'
+								className={classes.link}
+							>profile</NavLink>
+							<NavLink 
+								to='/travels'
+								className={classes.link}
+							>travels</NavLink>
+						</>
+					:
+						<>
+							<NavLink 
+								to='/regiser'
+								className={classes.link}
+							>register</NavLink>
+							<NavLink 
+								to='/login'
+								className={classes.link}
+							>login</NavLink>
+						</>
+				}
+			</div>
 		</div>
 	)
 }
