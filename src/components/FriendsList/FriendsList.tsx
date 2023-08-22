1ff512e9a2b6faa48fb658af5bb169d8a1c47d53
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-22 21:26:33 +0300

    added initial friends list

---
+++ src/components/FriendsList/FriendsList.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+import classes from './friends-list.module.css';
+
+const FriendsList: FC = () => {
+	return (
+		<div>
+			friends list
+		</div>
+	)
+}
+
+export default FriendsList;
