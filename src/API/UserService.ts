b60e2a9e260a00fcee3f8ddd076e0da86d272d24
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-22 11:52:50 +0300

    added send friend request method

--- src/API/UserService.ts
+++ src/API/UserService.ts
@@ -21,4 +21,17 @@ export default class UserService {
 		};
 		axios.patch(`http://localhost:8080/users/${user.id}`, body);
 	}
+
+	static async getAllUsers(): Promise<IUser[]> {
+		const response = await axios.get('http://localhost:8080/users')
+		return response.data;
+	}
+
+	static sendFriendRequest(toId: any, fromData: any): void {
+		axios.get(`http://localhost:8080/users/${toId}`).then(response => {
+			const allRequests = response.data.friendRequests;
+			const body = {"friendRequests": [...allRequests, fromData]}
+			axios.patch(`http://localhost:8080/users/${toId}`, body);
+		})
+	}
 }
