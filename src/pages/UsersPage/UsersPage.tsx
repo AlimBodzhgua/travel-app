25e481cbe9b4f564c6dec8a9e46c9cfc9359ac03
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-24 22:50:59 +0300

    added loader and error handle

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,7 +1,8 @@
-import {FC, useEffect, useState} from 'react';
+import { FC, useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from 'hooks/redux';
 import { fetchAllUsers } from 'redux/actions/allUsersActions';
 import { selectUser, selectAllUsers } from 'redux/selectors/selectors';
+import { RotatingLines } from 'react-loader-spinner';
 import NavBar from 'components/Navbar/NavBar';
 import UsersList from 'components/UsersList/UsersList';
 import classes from './users.module.css';
@@ -9,6 +10,7 @@ import classes from './users.module.css';
 const UsersPage: FC = () => {
 	const user = useAppSelector(selectUser);
 	const users = useAppSelector(state => selectAllUsers(state, Number(user?.id)));
+	const {isLoading, errorMessage} = useAppSelector(state => state.allUsersReducer);
 	const dispatch = useAppDispatch();
 
 	useEffect(() => {
@@ -18,10 +20,24 @@ const UsersPage: FC = () => {
 	return (
 		<div className={classes.container}>
 			<NavBar />
-			<h2 className={classes.title}>Travelers</h2>
-			{users.length 
-				? <UsersList users={users} />
-				: <h2>No users here yet</h2>
+			{errorMessage 
+				?	<h2>Error loading users, reload the page or try later</h2>
+				:	<>	
+						<h2 className={classes.title}>Travelers</h2>
+						{users.length 
+							? isLoading 
+								? 	<div className={classes.loader}>
+										<RotatingLines
+						                    strokeColor='grey'
+						                    strokeWidth='5'
+						                    animationDuration='0.75'
+						                    width='55'
+						                />
+						            </div>
+								:	<UsersList users={users} /> 
+							: <h2>No users here yet</h2>
+						}
+					</>
 			}
 		</div>
 	)
