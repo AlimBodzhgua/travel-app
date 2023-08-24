c929a1c0ca088f0f1daec2c7bd13b5723fa8de47
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-24 22:29:57 +0300

    added generic types for actions

--- src/redux/actions/userActions.ts
+++ src/redux/actions/userActions.ts
@@ -3,9 +3,13 @@ import { IUser, IUserLogin } from 'types/types';
 import { getErrorMessage, modifyUserResponseObject, saveUserToLocalStorage } from 'utils/utils';
 import UserService from 'API/UserService';
 
-export const registerUser = createAsyncThunk(
+export const registerUser = createAsyncThunk<
+	IUser, 
+	IUser, 
+	{rejectValue: string}
+>(
 	'users/register',
-	async (data: IUser, {rejectWithValue}) => {
+	async (data, {rejectWithValue}) => {
 		try {
 			const response = await UserService.register(data);
 			const user = modifyUserResponseObject(response);
@@ -17,16 +21,20 @@ export const registerUser = createAsyncThunk(
 	}
 );
 
-export const loginUser = createAsyncThunk(
+export const loginUser = createAsyncThunk<
+	IUser,
+	IUserLogin,
+	{rejectValue: string}
+>(
 	'users/login',
-	async (data: IUserLogin, {rejectWithValue}) => {
+	async (data, {rejectWithValue}) => {
 		try {
 			const response = await UserService.login(data);
 			const user = modifyUserResponseObject(response);
 			saveUserToLocalStorage(user);
 			return user;
 		} catch (e) {
-			rejectWithValue(getErrorMessage(e));
+			return rejectWithValue(getErrorMessage(e));
 		}
 	}
 );
