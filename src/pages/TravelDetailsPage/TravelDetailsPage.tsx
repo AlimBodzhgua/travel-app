24d0070ae8bfe08cb621fef9415d714fb808957d
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-27 17:56:14 +0300

    deleted unused variable

--- src/pages/TravelDetailsPage/TravelDetailsPage.tsx
+++ src/pages/TravelDetailsPage/TravelDetailsPage.tsx
@@ -3,6 +3,7 @@ import {useParams} from 'react-router-dom';
 import {useAppSelector} from 'hooks/redux';
 import { selectTravelById } from 'redux/selectors/selectors';
 import NavBar from 'components/Navbar/NavBar';
+import Members from 'components/Members/Members';
 import BacklogList from 'components/BacklogList/BacklogList';
 import Groups from 'components/Groups/Groups';
 import TravelItem from 'components/travel/TravelItem';
@@ -10,7 +11,6 @@ import classes from './travel-details.module.css';
 
 const TravelDetailsPage: FC = () => {
 	const { id } = useParams<{id? : string}>();
-	const { user } = useAppSelector(state => state.userReducer);
 	const travel = useAppSelector(state => selectTravelById(state, Number(id)));
 
 	return (
@@ -25,6 +25,7 @@ const TravelDetailsPage: FC = () => {
 							dateStart={travel.dateStart}
 							dateEnd={travel.dateEnd}
 						/>
+						<Members />
 						<div className={classes.details}>
 							<BacklogList />
 							<Groups />
