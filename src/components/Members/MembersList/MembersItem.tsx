88a2818187494042624b585f99ffd4dcec13e007
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-27 11:11:17 +0300

    added delete member action

--- src/components/Members/MembersList/MembersItem.tsx
+++ src/components/Members/MembersList/MembersItem.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from 'hooks/redux';
 import { useParams } from 'react-router-dom';
 import { IFriend } from 'types/types';
 import { useHover } from 'hooks/useHover';
+import { userSlice } from 'redux/reducers/userSlice';
 import classes from './members-list.module.css';
 
 interface MembersItemProps {
@@ -10,7 +11,16 @@ interface MembersItemProps {
 }
 
 const MembersItem: FC<MembersItemProps> = ({member}) => {
+	const { id } = useParams<{id?: string}>();
 	const [hovering, hoverProps]  = useHover();
+	const dispatch = useAppDispatch();
+
+	const handleClick = ():void => {
+		dispatch(userSlice.actions.deleteMember({
+			travelId: Number(id),
+			memberId: member.id
+		}))
+	}
 
 	return (
 		<li className={classes.item} {...hoverProps}>
@@ -21,6 +31,7 @@ const MembersItem: FC<MembersItemProps> = ({member}) => {
 			<button 
 				style={{transform: hovering ? 'scale(1.0)' : 'scale(0)'}}
 				className={classes.cancel} 
+				onClick={handleClick}
 			>&#10005;</button>
 		</li>
 	)
