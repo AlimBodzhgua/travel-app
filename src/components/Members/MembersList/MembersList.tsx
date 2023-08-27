473b08376c3e364bd38d3da3fd095465afb9a601
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-27 10:18:43 +0300

    added droppable area

--- src/components/Members/MembersList/MembersList.tsx
+++ src/components/Members/MembersList/MembersList.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react';
-import {IFriend} from 'types/types';
+import { IFriend } from 'types/types';
+import { DragOverlay } from '@dnd-kit/core';
 import MembersItem from './MembersItem';
+import Item from 'components/AddMembersForm/Item';
+import DroppableMembersArea from 'components/AddMembersForm/DroppableMembersArea';
+
 import classes from './members-list.module.css';
 
 interface MembersListProps {
 	members: IFriend[];
+	activeItem: IFriend | null
 }
 
-const MembersList: FC<MembersListProps> = ({members}) => {
+const MembersList: FC<MembersListProps> = ({members, activeItem}) => {
 	return (
 		<ul className={classes.list}>
 			{members.map(member => 
@@ -16,6 +21,17 @@ const MembersList: FC<MembersListProps> = ({members}) => {
 					member={member}
 				/>
 			)}
+			{activeItem && 	
+				<>
+					<DragOverlay>
+						{activeItem 
+							? 	<Item friend={activeItem} />
+							: 	null
+						}
+					</DragOverlay>
+					<DroppableMembersArea />
+				</>
+			}
 		</ul>
 	)
 }
