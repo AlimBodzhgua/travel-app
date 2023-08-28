6ed424dfc853f25e2d7a3f8244e0a0d05ccb7a9f
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-08-28 11:05:46 +0300

    added removeMembersFromFriendList helper

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,13 @@
-import {IUser, ITravel, IUserResponse, IGroup, ICard} from 'types/types';
+import {
+	IUser, 
+	ITravel, 
+	IUserResponse, 
+	IGroup, 
+	ICard,
+	IFriend,
+} from 'types/types';
 import dayjs, {Dayjs} from 'dayjs';
+
 
 export const getErrorMessage = (error: unknown):string => {
 	if (error instanceof Error) return error.message;
@@ -18,17 +26,6 @@ export const createNewUser =
 	;
 }
 
-export const saveUserToLocalStorage = (user: IUser):void => {
-	localStorage.setItem('user', JSON.stringify(user));
-};
-
-export const isUserLoggedIn = ():boolean => {
-	return localStorage.hasOwnProperty('user');
-};
-
-export const modifyUserResponseObject = (data: IUserResponse):IUser => {
-	return {...data.user, password: data.accessToken};
-};
 
 export const createNewTravel = (
 	id: number, 
@@ -47,15 +44,41 @@ export const createNewTravel = (
 	};
 };
 
-export const stringToDayjsObject = (date: string):Dayjs => {
-	const jsDateObj = dayjs(date).toDate();
-	return dayjs(jsDateObj);
-};
 
 export const createNewGroup = (title: string):IGroup => {
 	return {id: Date.now(), title, cards: []};
 };
 
+
 export const createNewCard = (title: string, description: string): ICard => {
 	return {id: Date.now(), title, description};
 };
+
+
+export const saveUserToLocalStorage = (user: IUser):void => {
+	localStorage.setItem('user', JSON.stringify(user));
+};
+
+
+export const isUserLoggedIn = ():boolean => {
+	return localStorage.hasOwnProperty('user');
+};
+
+
+export const modifyUserResponseObject = (data: IUserResponse):IUser => {
+	return {...data.user, password: data.accessToken};
+};
+
+
+export const stringToDayjsObject = (date: string):Dayjs => {
+	const jsDateObj = dayjs(date).toDate();
+	return dayjs(jsDateObj);
+};
+
+
+export const removeMembersFromFriendList = (members: IFriend[], friends: IFriend[]): IFriend[] => {
+	return friends.filter((friend) => {
+		const find = members.find(member => member.id === friend.id);
+		if (!find) return friend
+	}) || []
+}
