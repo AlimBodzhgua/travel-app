1928b1723437f7322cc0b0a27209058a7f086c92
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-10-06 20:17:41 +0300

    route paths changed from string to RouteNames enum values

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,5 +1,6 @@
 import { FC, memo } from 'react';
 import { NavLink } from 'react-router-dom';
+import { RouteNames } from 'router/routes';
 
 import classes from './navbar.module.css';
 
@@ -8,23 +9,38 @@ const NavBar: FC = () => {
 		<nav className={classes.nav}>
 			<div className={classes.nav__left}>
 				<h1 className={classes.logo}>
-					<NavLink className={classes.logo__link} to='/'>
+					<NavLink
+						className={classes.logo__link}
+						to={RouteNames.HOME}
+					>
 						Travel 
 						<span className={classes.logo__blue}>Pro</span>
 					</NavLink>
 				</h1>
-				<NavLink to='/travels' className={classes.nav__link}>
+				<NavLink
+					to={RouteNames.TRAVELS}
+					className={classes.nav__link}
+				>
 					My Travels
 				</NavLink>
-				<NavLink to='/users' className={classes.nav__link}>
+				<NavLink
+					to={RouteNames.USERS}
+					className={classes.nav__link}
+				>
 					Users
 				</NavLink>
 			</div>	
 			<div className={classes.nav__right}>		
-				<NavLink to='/friends' className={classes.nav__link}>
+				<NavLink
+					to={RouteNames.FRIENDS}
+					className={classes.nav__link}
+				>
 					Friends
 				</NavLink>
-				<NavLink to='/profile' className={classes.nav__link}>
+				<NavLink
+					to={RouteNames.PROFILE}
+					className={classes.nav__link}
+				>
 					Profile
 				</NavLink>
 			</div>
