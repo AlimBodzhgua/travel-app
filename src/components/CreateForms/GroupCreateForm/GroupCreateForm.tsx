f0b9f54c1fe3506a540fee1192a1c7689e468e81
Alim <bodzhgua2015@mail.ru>
2025-01-07 13:31:37 +0300

    changed input to sutrom ui component, added success icon, added hotkey interaction

--- src/components/CreateForms/GroupCreateForm/GroupCreateForm.tsx
+++ src/components/CreateForms/GroupCreateForm/GroupCreateForm.tsx
@@ -1,53 +1,80 @@
-import { FC, useState, memo } from 'react';
+import { FC, useState, memo, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch } from 'hooks/redux';
 import { userActions } from 'redux/reducers/userSlice';
-import {createNewGroup} from 'utils/utils';
+import { createNewGroup } from 'utils/utils';
+import { Input } from 'components/UI/Input/Input';
+import { Button, ButtonTheme } from 'components/UI/Button/Button';
+import { ReactComponent as SuccessIcon } from 'assets/icons/success.svg';
+
 import classes from './group-create.module.css';
 
 interface GroupCreateFormProps {
-	setShowCreateForm?: React.Dispatch<React.SetStateAction<boolean>>
+	onCancel: () => void;
 }
 
-export const GroupCreateForm: FC<GroupCreateFormProps> = memo(({setShowCreateForm}) => {
-	const [value, setValue] = useState<string>('');
+export const GroupCreateForm: FC<GroupCreateFormProps> = memo((props) => {
+	const { onCancel } = props;
 	const { id } = useParams<{id? : string}>();
+	const [value, setValue] = useState<string>('');
 	const dispatch = useAppDispatch();
 
-	const handleSaveClick = ():void => {
-		if (value.length) {
-			const group = createNewGroup(value);
-			dispatch(userActions.addGroup({id: Number(id), group}));
-			setShowCreateForm?.(false);
-		} else alert('Empty input value');
+	const onHotkeyPress = (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			onSave();
+		} else if (e.key === 'Escape') {
+			onCancel();
+		}
 	};
 
-	const handleCancelClick = () => setShowCreateForm?.(false);
+	useEffect(() => {
+		window.addEventListener('keydown', onHotkeyPress);
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		return () => window.removeEventListener('keydown', onHotkeyPress);
+	}, []);
+
+	const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setValue(e.target.value);
+	};
+
+	const onSave = () => {
+		if (value.length) {
+			const group = createNewGroup(value);
+			console.log(value);
+			dispatch(userActions.addGroup({ id: Number(id), group }));
+			onCancel();
+		} else alert('Empty input value');
 	};
 
 	return (
 		<div className={classes.form}>
-			<input 
-				autoFocus
-				className={classes.form__input}
+			<Input
 				value={value}
-				onChange={handleInputChange}
-				type='text' 
+				onChange={onChangeValue}
+				className={classes.input}
+				autoFocus
+				size='md'
+				type='text'
 				placeholder='Enter group name'
+				addonAfter={
+					<div className={classes.actions}>
+						<Button
+							onClick={onSave}
+							className={classes.saveBtn}
+							theme={ButtonTheme.CLEAR}
+						>
+							<SuccessIcon className={classes.icon} />
+						</Button>
+						<Button
+							onClick={onCancel}
+							className={classes.cancelBtn}
+							theme={ButtonTheme.CLEAR}
+						>
+							&#10005;
+						</Button>
+					</div>
+				}
 			/>
-			<div className={classes.form__actions}>
-				<button 
-					onClick={handleSaveClick}
-					className={classes.save}
-				>&#x2714;</button>
-				<button 
-					onClick={handleCancelClick}
-					className={classes.cancel}
-				>&#10005;</button>
-			</div>
 		</div>
 	);
 });
