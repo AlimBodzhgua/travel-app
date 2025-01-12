5c99fc7b720e72183122ad33e7df3785c0cb9d37
Alim <bodzhgua2015@mail.ru>
2025-01-12 12:44:06 +0300

    changed handler and class names

--- src/components/Friends/FriendItem/FriendItem.tsx
+++ src/components/Friends/FriendItem/FriendItem.tsx
@@ -1,9 +1,9 @@
 import { FC, memo } from 'react';
-import { IFriend } from 'types/types';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import { selectUser } from 'redux/selectors/selectors';
 import { deleteFriend } from 'redux/actions/userActions';
 import { Button } from 'components/UI/Button/Button';
+import type { IFriend } from 'types/types';
 import classnames from 'classnames';
 import classes from './FriendItem.module.css';
 
@@ -12,11 +12,12 @@ interface FriendItemProps {
 	className?: string;
 }
 
-export const FriendItem: FC<FriendItemProps> = memo(({friend, className}) => {
+export const FriendItem: FC<FriendItemProps> = memo((props) => {
+	const { friend, className } = props;
 	const user = useAppSelector(selectUser);
 	const dispatch = useAppDispatch();
 
-	const handleClick = ():void => {
+	const onDelete = () => {
 		if (window.confirm('Are you sure you want to delete friend?')) {
 			if (user) {
 				dispatch(deleteFriend({
@@ -34,8 +35,8 @@ export const FriendItem: FC<FriendItemProps> = memo(({friend, className}) => {
 				<div>{friend.login}</div>
 			</div>
 			<Button 
-				onClick={handleClick}
-				className={classes.delete}
+				onClick={onDelete}
+				className={classes.deleteBtn}
 				theme='red'
 				size='sm'
 			>
