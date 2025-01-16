a29f35251df264ef390e9d591b6c2aa6549cfa7c
Alim <bodzhgua2015@mail.ru>
2025-01-16 12:49:34 +0300

    lint fix

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,25 +1,25 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
 import { StyleDecorator } from '../src/config/storybook/StyleDecorator/StyleDecorator';
 import { BrowserDecorator } from '../src/config/storybook/BrowserDecorator/BrowserDecorator';
 import { LocalizationDecorator } from '../src/config/storybook/LocalizationDecorator/LocalizationDecorator';
 import { StoreDecorator } from '../src/config/storybook/StoreDecorator/StoreDecorator';
 
 const preview: Preview = {
-    parameters: {
-        actions: { argTypesRegex: "^on[A-Z].*" },
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/,
-            },
-        },
-    },
-    decorators: [
-        StyleDecorator,
-        BrowserDecorator,
-        LocalizationDecorator,
-        StoreDecorator({user: { authData: undefined }}),
-    ],
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+	},
+	decorators: [
+		StyleDecorator,
+		BrowserDecorator,
+		LocalizationDecorator,
+		StoreDecorator({ user: { authData: undefined } }),
+	],
 };
 
 export default preview;
